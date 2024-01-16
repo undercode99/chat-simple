7058a48e0c874e97e852e920093764167aeaf248
undercode99 <nim4n136@gmail.com>
2024-01-16 19:12:40 +0700

    feat: setup lib and utilities

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useCounterStore } from './stores/counter'
+const counter = useCounterStore()
+const increment = () => {
+  counter.increment()
+}
+</script>
+
+<template>
+  <div class="text-red-500">
+    <p>Chat Simple</p>
+    <button
+      @click="increment"
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      Increment +{{ counter.count }}
+    </button>
+  </div>
+</template>
